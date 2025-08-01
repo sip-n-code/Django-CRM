45fd3515318968a67cc0a7539a56fa7f034173a7
sip-n-code <sipholukhele1994@gmail.com>
2025-08-01 18:46:56 +0200

    Extended security

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -13,7 +13,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^%5tsp+!is(8f^%$6(6cavx-d)1d7dhz2#-&x2zf2tm@iisw-m"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
