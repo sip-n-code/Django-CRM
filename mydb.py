945ad839265ed2f324b1bca93d3a57b9cf9e748d
sip-n-code <sipholukhele1994@gmail.com>
2025-08-01 18:42:20 +0200

    Updated security

--- mydb.py
+++ mydb.py
@@ -1,10 +1,18 @@
+import os
 import mysql.connector
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+
+# If .env is in the same folder as this script
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 dataBase = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="StupidMysql10",
-    # database="elderco"
+    host=os.getenv("DB_HOST", "127.0.0.1"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
 )
 
 # prepare a cursor object using cursor() method
